8766062ef737c35cbf367560119bbefebec9d587
eKhafizov <130173250+eKhafizov@users.noreply.github.com>
2025-05-09 23:10:31 +0200

    test 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';        // <-- импортируем createRoot
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
+import './index.css';                                 // если у вас есть глобальные стили
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+// Берём контейнер из index.html
+const container = document.getElementById('root');
+
+// Создаём корневой объект
+const root = createRoot(container);
+
+// Рендерим приложение
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
