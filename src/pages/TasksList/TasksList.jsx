e6589e5a7963b52d7351c8f14da72c44be3a5b6f
eKhafizov <130173250+eKhafizov@users.noreply.github.com>
2025-05-15 22:08:15 +0200

    xxxx

--- src/pages/TasksList/TasksList.jsx
+++ src/pages/TasksList/TasksList.jsx
@@ -2,18 +2,44 @@
 import React from 'react'
 import { useFetchTasksQuery } from '../../store/api'
 import CreateTask from '../../components/CreateTask/CreateTask'
-import styles from './TasksList.module.css'
 
 export default function TasksList() {
 
   const { data: tasks = [], error, isLoading, refetch } = useFetchTasksQuery()
 
   if (isLoading) return <p>Загрузка задач…</p>
-  if (error)     return <p>Ошибка при загрузке: {error.toString()}</p>
+  if (error) {
+    return (
+      <p>
+        Ошибка загрузки: {
+          // Если fetchBaseQuery вернул JSON с detail
+          error.data?.detail
+          // Либо строковое сообщение
+          ?? (typeof error.data === 'string' && error.data)
+          // Либо внутренняя ошибка
+          ?? error.error
+          // Либо просто приведём весь объект к строке (на крайний случай)
+          ?? JSON.stringify(error)
+        }
+      </p>
+  )}
 
   return (
     <>
       <CreateTask onCreated={refetch} />
+      {!isLoading && !error && (
+        <ul >
+          {tasks.map(task => (
+            <li key={task.id} >
+              <strong>{task.title}</strong>
+              {task.description && <p>{task.description}</p>}
+              <p>
+                Статус: {task.is_completed ? '✅ Выполнена' : '❌ Не выполнена'}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
       <ul>
         {tasks.map(task => (
           <li key={task.id}>
