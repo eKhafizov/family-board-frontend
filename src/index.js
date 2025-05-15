a51bc7655470dddfcc26a7dc2d0d042eefd8757c
eKhafizov <130173250+eKhafizov@users.noreply.github.com>
2025-05-15 20:01:55 +0200

    prev worked

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';        // <-- импортируем createRoot
 import { Provider } from 'react-redux';
-import { store } from './store';
 import App from './App';
-import './index.css';                                 // если у вас есть глобальные стили
+import './index.css'; 
+import { store } from './store/index'         // ← импорт store
+import { api }   from './store/apiSlice'// ← импорт apiSlice
 
 // Берём контейнер из index.html
 const container = document.getElementById('root');
 
 // Создаём корневой объект
 const root = createRoot(container);
+
+// Делаете их доступными в консоли
+window.store = store
+window.api   = api
 
 // Рендерим приложение
 root.render(
